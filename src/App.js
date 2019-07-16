4868edd7852693768f8e63d126ac9ff0368fdaf5
lilyhoratio <lilyhzhou@gmail.com>
2019-07-16 14:16:35 -0700

    removed <Numberbutton> from App.js bc it is only needed for numbers to get button styling

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ function App() {
       <div className="App">
         {/* STEP 4 - Render your components here and be sure to properly import/export all files */}
         <Numbers></Numbers>
-        <NumberButton></NumberButton>
+        {/* <NumberButton></NumberButton> */}
       </div>
     </div>
   );
