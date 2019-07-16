05759736cb90e48f6e19b100d779fef7c151ef0b
lilyhoratio <lilyhzhou@gmail.com>
2019-07-16 13:55:53 -0700

    added App.scss for styling

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
 import "./App.css";
-// import ".App.scss";
+import "./App.scss";
 
 // STEP 4 - import the button and display components
 // Don't forget to import any extra css/scss files you build into the correct component
 // Logo has already been provided for you. Do the same for the remaining components
 import Logo from "./components/DisplayComponents/Logo";
-import NumberButton from "./components/ButtonComponents/NumberButtons/NumberButton.js";
+// import NumberButton from "./components/ButtonComponents/NumberButtons/NumberButton.js";
 
 
 function App() {
