ff2dc309d631c43e25ca5363fb16ef9e9f83e78b
lilyhoratio <lilyhzhou@gmail.com>
2019-07-16 14:28:01 -0700

    setting up scss for button styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
-import "./App.css";
+// import "./App.css";
 import "./App.scss";
 
 // STEP 4 - import the button and display components
 // Don't forget to import any extra css/scss files you build into the correct component
 // Logo has already been provided for you. Do the same for the remaining components
 import Logo from "./components/DisplayComponents/Logo";
+import Display from "./components/DisplayComponents/Display.js"
 import NumberButton from "./components/ButtonComponents/NumberButtons/NumberButton.js";
 import Numbers from "./components/ButtonComponents/NumberButtons/Numbers.js";
 
@@ -21,6 +22,8 @@ function App() {
       <Logo />
       <div className="App">
         {/* STEP 4 - Render your components here and be sure to properly import/export all files */}
+        {/* <Display className="display"></Display> */}
+        <Display></Display>
         <Numbers></Numbers>
         {/* <NumberButton></NumberButton> */}
       </div>
--- src/components/ButtonComponents/NumberButtons/NumberButton.js
+++ src/components/ButtonComponents/NumberButtons/NumberButton.js
@@ -5,7 +5,7 @@ import React from "react";
 const NumberButton = (props) => {
   console.log("props", props.number)
   return (
-    <button>
+    <button className="calc-button number-button">
       {/* Display a button element rendering the data being passed down from the parent container on props */
       props.number}
     </button>
